1515d841e722195c2bed731e30d80c7d7ab257c7
Paulino101 <89439038+Paulino101@users.noreply.github.com>
2022-04-21 15:18:58 -0700

    converted entries to .map fx

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Introduction from "./components/Introduction";
 import NavBar from "./components/NavBar";
 // React Router
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Authentication from "./AuthenticationSection/Authentication";
+
+import AuthenticationEntries from "./AuthenticationSection/Authentication";
+// entries
+const authenticationEntries = [
+  {
+    title: "Making the UI",
+    date: "April 20,2022",
+    body: "Not much to talk about this part of authentication I made a simple form that has 2 inputs and a submit button.",
+    img: "www.placeholder.com",
+    imgCaption: "caption about the image",
+  },
+  {
+    title: "Firebase",
+    date: "April 20,2022",
+    body: "After making the inputs it was time to add functionality. I chose to use Firebase because I loved their documentation. Also there was a lot of support online if I ever got stuck on something(which I did, a lot). I spent a good week implementing the email sign in and an extra week implementing the google sign in.",
+    img: "www.placeholder.com",
+    imgCaption: "caption about the image",
+  },
+];
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/authentication" element={<Authentication />} />
+        <Route
+          path="/authentication"
+          element={<AuthenticationEntries entries={authenticationEntries} />}
+        />
         <Route path="/" element={<Introduction />} />
       </Routes>
     </Router>
--- src/AuthenticationSection/Authentication.jsx
+++ src/AuthenticationSection/Authentication.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
-function Authentication() {
+function AuthenticationEntries({entries}) {
   return (
       <>
-          <h1 className="mt-5">Authentication</h1>
-          <section id='ui'>
-              <h2>Making the UI</h2>
-             <p>Not much to talk about this part of authentication I made a simple form that has 2 inputs and a submit button.</p>
-            {/* need and image with caption */}
-          </section>
-          <section id="firebase">
-              <h2>FireBase</h2>
-              <p>After making the inputs it was time to add functionality. I chose to use Firebase because I loved their documentation. Also there was a lot of support online if I ever got stuck on something(which I did, a lot). I spent a good week implementing the email sign in and an extra week implementing the google sign in.</p>
-
-          </section>
+          <h1 className="m-5">Authentication</h1>
+          {entries.map(entry => (
+            <section className="ms-3">
+                  <h2>{entry.title}</h2>
+                  <p className="text-right font-weight-bold"> {entry.date}</p>
+                  <p>{entry.body}</p>
+                  {/* need img and img cap here still */}
+                  
+              </section>
+          ))}
       </>
   );
 }
 
-export default Authentication;
+export default AuthenticationEntries;
